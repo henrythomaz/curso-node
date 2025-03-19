3ee1893cd0fce7107c0dbe7529c0a1cb995141d5
Henry <henrytoliveriacampos@gmail.com>
2025-03-19 11:53:01 -0300

    Desfazendo umas cagadas

--- exercícios/ex017/script.js
+++ exercícios/ex017/script.js
@@ -1,17 +1,5 @@
-var c = 1
-while (c <= 5) {
-    console.log(c)
+var c = 1;
+while (c < 500) {
+    console.log(`Passo${c}`)
     c++
 }
-// ----------------
-
-var c = 1
-do {
-    console.log(c)
-} while (c <= 5)
-// ----------------
-
-for (var c= 1; c <= 5; c++) {
-    console.log(c)
-}
-// ----------------
