235cd40d56fcc039dfb1644e64fe35346770c7ff
Henry <henrytoliveriacampos@gmail.com>
2025-03-28 13:06:10 -0300

    Arrumando a desestruturação simples

--- exercícios/ex025/script.js
+++ exercícios/ex025/script.js
@@ -3,17 +3,13 @@
 function main() {
     const person = {
         name: "Henry",
-        idade: 15
+        idade: 15,
+        isProgrammer: true
     };
-    const name = person.name;
-    const { name2 } = person;
+    // const name = person.name; X
+    const { name, idade, isProgrammer } = person;
+    console.log(name, idade, isProgrammer)
 };
 main();
 
-const array = ["Henry", 15, "Campos"];
 
-const [firstName, idade, lastName] = array;
-
-console.log(firstName, idade, lastName);
-
-const {firstName: name} = player;
---
+++ exercícios/ex026/script.js
@@ -0,0 +1,10 @@
+// Desestruturação de Array
+
+const array = ["Henry", 15, "Campos"];
+
+const [firstName, idade, lastName] = array;
+
+console.log(firstName, idade, lastName);
+
+// Muda o nome
+const {firstName: name} = player;
