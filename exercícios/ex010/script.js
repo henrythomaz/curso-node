22044c4c6a88ce7645fcdd0b069a572fe09683a0
Henry <henrytoliveriacampos>
2025-04-04 14:47:58 -0400

    Modificando o ex010

--- exercícios/ex010/script.js
+++ exercícios/ex010/script.js
@@ -3,7 +3,7 @@ var txt = window.document.getElementById('pais')
 var pais = txt.value.toUpperCase()
 
 var res = window.document.getElementById('res')
-    res.innerHTML += `Vivendo no ${pais}`
+    res.innerHTML = `Vivendo no ${pais}`
 
     if (pais == 'BRASIL' || pais == 'BRAZIL') {
         res.innerHTML += '<br> brasileiro!'
