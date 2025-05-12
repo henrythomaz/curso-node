f987d4a215cd28e6e8fa1dcc9922d35576bf09c4
= <=>
2025-05-12 10:28:59 -0400

    Começando o mini projeto

---
+++ exercicios-node/ex008/public/JS/script.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', () => {
+  enviarLink();
+});
+
+function enviarLink() {
+  const link = document.getElementById("link").value;
+  const name = document.getElementById("name").value;
+  const res = document.getElementById("res").value;
+
+  
+}
